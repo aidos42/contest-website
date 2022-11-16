993a1ae9d1ec2db3a563d08bd6493e3ab63f0d56
aidos42 <75936428+aidos42@users.noreply.github.com>
2022-11-16 12:49:11 +0600

    add basic routing and static content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const filename = fileURLToPath(import.meta.url);
+const customDirname = dirname(filename);
+
+const PORT = process.env.port ?? 4242;
 
 const app = express();
 
-app.listen(4242);
+app.set('view engine', 'pug');
+app.set('views', path.join(customDirname, '/views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(customDirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/new-clip', (req, res) => {
+  console.log(req.body);
+  res.redirect(303, '/');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on port: ${PORT}`);
+});
